5715e4ec6744893af2fae33554edd26dc5e4a5f0
Brice Archenault <contact@bricearchenault.com>
2016-09-13 01:08:41 +0200

    fix responsive table status

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
 
 	// Select Box
-	// $('.js-fancySelect').fancySelect({
-	// 	includeBlank: true
-	// });
-
 	var select = $('.js-fancySelect');
 	select.fancySelect().on('change.fs', function() {
 	    var url = $(this).val();
@@ -13,7 +9,6 @@ $(document).ready(function() {
           }
           return false;
 	});
-
 	//
 
 
@@ -25,11 +20,58 @@ $(document).ready(function() {
 	//
 
 	//
-	//
+	// Footer collapse icons
 	$(".js-footerCollapse").click(function() {
 		$(this).find("i").toggleClass("hidden");
 	})
 	//
+
+
+	//
+	// Responsive table to SLIDER table
+
+		
+		// VARS
+		var windowWidth = $(window).width();
+
+		if (windowWidth < 768) {
+
+			$(".productTable-column").css("width", windowWidth);
+
+			var calcx5 = windowWidth*5;
+			$(".productTable-table").css("width", calcx5);
+
+
+
+			// $(window).resize(function() {
+			// 	$(".productTable-column").css("width", windowWidth);
+
+			// 	var calcx5 = windowWidth*5;
+			// 	$(".productTable-table").css("width", calcx5);
+			// });
+
+
+
+			//
+			$('.js-nextColumn').click(function(e) {
+		        e.preventDefault();
+		        $('.productTable-wrapper').animate(
+		        	{ scrollLeft:'+='+windowWidth }, 'slow'
+		        );        
+		    });
+		    $('.js-prevColumn').click(function(e) {
+		        e.preventDefault();
+		        $('.productTable-wrapper').animate(
+		        	{ scrollLeft:'-='+windowWidth }, 'slow'
+		        );        
+		    });
+
+		}
+		
+	
+
+	//
+
 
 
 	//
@@ -54,4 +96,16 @@ $(document).ready(function() {
 	});
 
 
+});
+
+
+
+$(window).resize(function() {
+	var windowWidth2 = $(window).width();
+	console.log("windowWidth2 : " + windowWidth2);
+
+	$(".productTable-column").css("width", windowWidth2+" !important");
+
+	var calcx5 = windowWidth2*5;
+	$(".productTable-table").css("width", calcx5);
 });
