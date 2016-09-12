2d1a4b11e7d0ce26d7f0d40d605204baa1144f01
Brice Archenault <contact@bricearchenault.com>
2016-09-12 12:00:51 +0200

    gix therapeutic areas page AND prescribing infos AND Cost saving calculator page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+	// Select Box
 	$('.js-fancySelect').fancySelect();
+
+
+	//
+	// Auto open collapse when ID is load in URL
+		var anchor = window.location.hash.replace("#", "");
+	    $(".collapse").collapse('hide');
+	    $("#" + anchor).collapse('show');
+	//
+
+
 
 });
