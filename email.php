6582049e40b8de6f94a9c4956d9845ed1b49e55f
MalabarDesign <jhubert@malabardesign.fr>
2016-10-03 15:20:26 +0200

    faq

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 
-    $toEmail = "thomas.louet@malabardesign.fr";
+    $toEmail = "hubert.julien@gmail.com";
     $mailHeaders = "From: Ethypharm UK : " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
 
     if(mail($toEmail, "New message from Ethypharm UK Website", "\n\nEmail: " . $_POST["email"] . "\n\nPhone: " . $_POST["phone"] . "\n\nType: " . $_POST["select"] . "\n\nMessage: \n" . $_POST["message"].$_POST['g-recaptcha-response'], $mailHeaders)) {
