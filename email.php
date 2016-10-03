9ef5bd8da6010fd2b54593ec90cc6fa74a7abb3b
MalabarDesign <jhubert@malabardesign.fr>
2016-10-03 16:16:38 +0200

    faq

--- email.php
+++ email.php
@@ -15,14 +15,16 @@
    }
        $secretKey = "6LcARggUAAAAAAto65tJkVx1-5UhlmXt42a3B2JX";
         $ip = $_SERVER['REMOTE_ADDR'];
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+		$url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip;
+		echo $url;
+        /*$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
 		
         $responseKeys = json_decode($response,true);
         if(intval($responseKeys["success"]) !== 1) {
           $send = false; echo "a";
         } else {
            $send = true;  echo "b";
-        }
+        }*/
 		
 
     if(mail($toEmail, "New message from Ethypharm UK Website", "\n\nEmail: " . $_POST["email"] . "\n\nPhone: " . $_POST["phone"] . "\n\nType: " . $_POST["select"] . "\n\nMessage: \n" . $_POST["message"], $mailHeaders)) {
